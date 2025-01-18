182e7753c2aa52fc9b215285cd3e0dde3099c804
MeenuKumari17 <m1798kumari@gmail.com>
2025-01-18 12:37:43 +0530

    added router

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from './config/mongodb.js'
+import authRouter from './routes/authRoutes.js'
 
 const app = express();
 const port =  process.env.PORT || 4000
@@ -13,6 +14,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}))
 
+
+// API Endpoints
 app.get('/', (req, res) => res.send("API working."));
+app.use('/api/auth', authRouter)
+
 
 app.listen(port, () => console.log(`Serve started on PORT: ${port}`))
