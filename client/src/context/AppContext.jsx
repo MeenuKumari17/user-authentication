09549fa15da4c604cedf33f7d71be984aa58033f
MeenuKumari17 <m1798kumari@gmail.com>
2025-02-02 23:01:04 +0530

    logout functionality and fixed navbar

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 export const AppContext = createContext();
@@ -9,6 +9,21 @@ export const AppContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [isLoggedin, setIsLoggedin] = useState(false)
     const [userData, setUserData] = useState(false)
+    const getAuthState = async () => {
+        try {
+            const data = await axios.get(backendUrl + '/api/auth/is-auth')
+            if (data.success) {
+                setIsLoggedin(true)
+                getUserData()
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    useEffect(() => {
+        getAuthState();
+    },[])
 
 
     const getUserData = async () => {
@@ -16,7 +31,7 @@ export const AppContextProvider = (props) => {
             const {data} = await axios.get(backendUrl + '/api/user/data')
             data.success ? setUserData(data.userData) : toast.error(data.message)
         } catch (error) {
-            toast.error(data.message)
+            toast.error(error.message)
         }
     }
 
