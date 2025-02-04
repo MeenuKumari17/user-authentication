ef80a9d55eea18f5eb122f27c3b12f15224b8e47
MeenuKumari17 <m1798kumari@gmail.com>
2025-02-04 16:18:34 +0530

    email verification page functionality

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -6,12 +6,14 @@ export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
 
+    axios.defaults.withCredentials = true;
+
     const backendUrl = import.meta.env.VITE_BACKEND_URL
     const [isLoggedin, setIsLoggedin] = useState(false)
     const [userData, setUserData] = useState(false)
     const getAuthState = async () => {
         try {
-            const data = await axios.get(backendUrl + '/api/auth/is-auth')
+            const {data} = await axios.get(backendUrl + '/api/auth/is-auth')
             if (data.success) {
                 setIsLoggedin(true)
                 getUserData()
